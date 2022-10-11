d894d95f76bad3a05af016389d22ffebc7679160
Remigijus Jodelis <remigijus@toptal.com>
2022-10-11 22:08:44 +0300

    Day 5,6 of 2019

---
+++ 2019/d05.nim
@@ -0,0 +1,28 @@
+# Advent of Code 2019 - Day 5
+
+import std/[strutils, sequtils], intcode
+
+type Data = seq[int]
+
+proc parseData(filename: string): Data =
+  readFile(filename).strip.split(',').mapIt(it.parseInt)
+
+
+proc partOne(data: Data): int =
+  var ic = Intcode(data)
+  let output = ic.run2(@[1])
+  for i in 0..output.len-2:
+    assert output[i] == 0
+  return output[^1]
+
+
+proc partTwo(data: Data): int =
+  var ic = Intcode(data)
+  let output = ic.run2(@[5])
+  assert output.len == 1
+  return output[^1]
+
+
+let data = parseData("inputs/05.txt")
+echo partOne(data)
+echo partTwo(data)
--- 2019/intcode.nim
+++ 2019/intcode.nim
@@ -1,10 +1,10 @@
-import strutils
+import std/[strutils]
 
 type Intcode* = seq[int]
 
 
 # run Intcode program and return memory value at address 0
-proc run*(ic: var Intcode): int =
+proc run1*(ic: var Intcode): int =
   var iptr: int
 
   while iptr < ic.len:
@@ -20,4 +20,71 @@ proc run*(ic: var Intcode): int =
     else:
       raise newException(AssertionDefect, format("Invalid code at $#: $#", iptr, ic[iptr]))
 
-  ic[0]
+  result = ic[0]
+
+
+proc parseOp(opcode: int): tuple[opcode: int, modes: seq[int]] =
+  result.opcode = opcode mod 100
+  result.modes = @[result.opcode] # 1st mode is the opcode
+  var val = opcode div 100
+  while val > 0:
+    result.modes.add val mod 10
+    val = val div 10
+  while result.modes.len < 4:
+    result.modes.add 0
+
+
+proc getArg(ic: Intcode, delta, iptr: int, modes: seq[int]): int =
+  let val = ic[iptr + delta]
+  case modes[delta]
+  of 0: result = ic[val]
+  of 1: result = val
+  else:
+    raise newException(AssertionDefect, format("Invalid opcode mode on $# ($#): $#", iptr, delta, modes[delta]))
+
+
+# run Intcode program and return outputs list
+proc run2*(ic: var Intcode, input: seq[int], debug = false): seq[int] =
+  var iptr, inptr: int
+
+  while iptr < ic.len:
+    let (opcode, modes) = parseOp(ic[iptr])
+    if debug:
+      echo (iptr, ic[iptr], opcode, modes, result)
+
+    case opcode
+    of 1: # add
+      ic[ic[iptr + 3]] = ic.getArg(1, iptr, modes) + ic.getArg(2, iptr, modes)
+      iptr.inc(4)
+    of 2: # multiply
+      ic[ic[iptr + 3]] = ic.getArg(1, iptr, modes) * ic.getArg(2, iptr, modes)
+      iptr.inc(4)
+    of 3: # input
+      ic[ic[iptr + 1]] = input[inptr]
+      iptr.inc(2)
+      inptr.inc
+    of 4: # output
+      result.add ic.getArg(1, iptr, modes)
+      iptr.inc(2)
+    of 5: # jump-if-true
+      if ic.getArg(1, iptr, modes) != 0:
+        iptr = ic.getArg(2, iptr, modes)
+      else:
+        iptr.inc(3)
+    of 6: # jump-if-false
+      if ic.getArg(1, iptr, modes) == 0:
+        iptr = ic.getArg(2, iptr, modes)
+      else:
+        iptr.inc(3)
+    of 7: # less than
+      let val = if ic.getArg(1, iptr, modes) < ic.getArg(2, iptr, modes): 1 else: 0
+      ic[ic[iptr + 3]] = val
+      iptr.inc(4)
+    of 8: # equals
+      let val = if ic.getArg(1, iptr, modes) == ic.getArg(2, iptr, modes): 1 else: 0
+      ic[ic[iptr + 3]] = val
+      iptr.inc(4)
+    of 99: # halt
+      break
+    else:
+      raise newException(AssertionDefect, format("Invalid code at $#: $#", iptr, ic[iptr]))
