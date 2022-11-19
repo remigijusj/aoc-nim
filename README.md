71de7ceb6c56b04c826a5768ef63be10283b2f95
Remigijus Jodelis <remigijus@toptal.com>
2022-11-19 22:05:53 +0200

    Improve Readme and aoc helper

--- README.md
+++ README.md
@@ -2,9 +2,31 @@
 
 Advent of Code in Nim
 
+My goal is to build self-contained solutions (no external libs) using clean and concise code.
+
 ## Running scripts
 
-Code compiles with the current (v1.6.0) Nim compiler, for example: `nim c -r d01`.
+All code compiles with the current (>= v1.6) Nim compiler, for example: `nim c -r d01`.
+
+To run the solutions:
+- years 2019-2021 just run dXX, which will read data file from input folder.
+- years from 2022 feed input to stdin, e.g. `dXX < input/dXX.txt`, or use the helper script.
+- to use data from clipboard: `pbpaste --lf | dXX`
+
+Helper script to download puzzles/input data and run/test solutions:
+```
+bin/aoc command [day] [year]
+```
+Available commands (for the given day/year):
+- p, puzzle - downloads the puzzle
+- i, input - downloads the input data file
+- t, template - copies the template as placeholder
+- a, all - runs all 3 above commands
+- r, run - compile and run the script
+- x, check - compile and run the scipt, check is answers match saved data
+
+Day and year are optional, default to current.
+Download commands need the session.txt file to be present.
 
 ### Structure
 
--- bin/aoc.nim
+++ bin/aoc.nim
@@ -2,17 +2,20 @@ import std/[httpclient, os, osproc, strformat, tempfiles, strutils, times]
 
 type YearDay = tuple[year, day: int]
 
-type Step = enum stP, stI, stT
-
 proc getYearDay(args: seq[string]): YearDay =
   let time = now().utc
   let day  = if args.len > 0: args[0].parseInt else: time.monthday
-  let year = if args.len > 1: args[1].parseInt else: time.year
+  var year = if args.len > 1: args[1].parseInt else: time.year
+  if year < 2000: year = 2000 + year
   result = (year, day)
 
 
+proc getPath(subpath: string): string =
+  result = absolutePath("../" & subpath, getAppDir())
+
+
 proc fetchAocPage(yd: YearDay, suffix: string = ""): string =
-  let cookie = readFile("../session.txt")
+  let cookie = readFile(getPath("session.txt"))
   var client = newHttpClient()
   client.headers = newHttpHeaders({"Cookie": "session=" & cookie})
   result = client.getContent(fmt"https://adventofcode.com/{yd.year}/day/{yd.day}" & suffix)
@@ -29,67 +32,93 @@ proc parsePuzzle(page: string): string =
 
 
 proc saveFile(data: string, yd: YearDay, folder: string) =
-  if data == "":
+  if data.len > 0:
+    createDir(getPath(fmt"{yd.year}/{folder}"))
+    writeFile(getPath(fmt"{yd.year}/{folder}/{yd.day:02}.txt"), data)
+  else:
     echo "No content, won't save: " & $(yd) & " " & folder
-    return
-  createDir(fmt"../{yd.year}/{folder}")
-  writeFile(fmt"../{yd.year}/{folder}/{yd.day:02}.txt", data)
 
 
 proc copyTemplate(yd: YearDay) =
-  createDir(fmt"../{yd.year}")
-  let target = fmt"../{yd.year}/d{yd.day:02}.nim"
+  createDir(getPath(fmt"{yd.year}"))
+  let target = getPath(fmt"{yd.year}/d{yd.day:02}.nim")
   if fileExists(target):
     echo "Code file already exists"
   else:
-    let code = readFile("template.nim")
+    let code = readFile(getPath("bin/template.nim"))
       .replace("{year}", $(yd.year)).replace("{day}", $(yd.day)).replace("{day:02}", fmt"{yd.day:02}")
     writeFile(target, code)
 
 
 proc prepAnswers(yd: YearDay) =
-  createDir(fmt"../{yd.year}/answers")
-  let target = fmt"../{yd.year}/answers/{yd.day:02}.txt"
+  createDir(getPath(fmt"{yd.year}/answers"))
+  let answers = getPath(fmt"{yd.year}/answers/{yd.day:02}.txt")
+  if not fileExists(answers):
+    writeFile(answers, "")
+
+
+proc compileProgram(yd: YearDay) =
+  echo fmt"Compiling d{yd.day:02}..."
+  let outputs = execProcess(fmt"nim c d{yd.day:02}.nim", getPath(fmt"{yd.year}"))
+  if not contains(outputs, "[SuccessX]"):
+    echo outputs
+
+
+proc runProgram(yd: YearDay, clipboard = false): string =
+  setCurrentDir(getPath(fmt"{yd.year}"))
+  if yd.year in 2019..2021:
+    execProcess(fmt"cmd /c d{yd.day:02}.exe")
+  elif clipboard:
+    execProcess(fmt"cmd /c pbpaste | d{yd.day:02}.exe")
+  else:
+    execProcess(fmt"cmd /c d{yd.day:02}.exe < inputs/{yd.day:02}.txt")
+
+
+proc testAnswers(yd: YearDay) =
+  let target = getPath(fmt"{yd.year}/answers/{yd.day:02}.txt")
   if not fileExists(target):
-    writeFile(target, "")
+    echo "No answers file"; return
+  if not fileExists(getPath(fmt"{yd.year}/d{yd.day:02}.exe")):
+    echo "No program file"; return
+
+  let answers = readFile(target).strip
+  let current = runProgram(yd).strip
+  echo (if answers == current: "OK" else: "Mismatch!\n[answers]\n" & answers & "\n[current]\n" & current)
 
 
 proc printHelp() =
-  echo "aoc a(all)|p(uzzle)|i(nput)|t(template) [day] [year]"
+  echo """
+Usage: aoc [pitcerx] [day] [year]
+  Commands: p(uzzle) | i(nput) | t(emplate) | c(ompile) | e(xample) | r(un) | x(check)
+  Warning: "e" command doesn't work in years 2019-2021"""
 
 
-proc runSteps(yd: YearDay, steps: varargs[Step]) =
+proc runSteps(steps: string, yd: YearDay) =
   for step in steps:
     case step:
-    of stP: fetchAocPage(yd).parsePuzzle.saveFile(yd, "puzzles")
-    of stI: fetchAocPage(yd, "/input").saveFile(yd, "inputs")
-    of stT: copyTemplate(yd); prepAnswers(yd)
+      of 'p': fetchAocPage(yd).parsePuzzle.saveFile(yd, "puzzles")
+      of 'i': fetchAocPage(yd, "/input").saveFile(yd, "inputs")
+      of 't': copyTemplate(yd); prepAnswers(yd)
+      of 'c': compileProgram(yd)
+      of 'e': echo runProgram(yd, true)
+      of 'r': echo runProgram(yd)
+      of 'x': testAnswers(yd)
+      else: discard
 
 
 # DEBUG: echo readFile(fmt"cache/{yd.year}{yd.day:02}.htm").parsePuzzle
-proc runCommand(args: seq[string]) =
-  if args.len == 0:
-    printHelp()
-    return
-
-  let cmd = args[0]
-  let yd = getYearDay(args[1..^1])
-  case cmd:
-  of "a", "all":
-    runSteps(yd, stP, stI, stT)
-  of "p", "puzzle":
-    runSteps(yd, stP)
-  of "i", "input":
-    runSteps(yd, stI)
-  of "t", "template":
-    runSteps(yd, stT)
+proc runCommands(args: seq[string]) =
+  if args.len > 0:
+    let cmd = args[0]
+    let yd = getYearDay(args[1..^1])
+    runSteps(cmd, yd)
   else:
     printHelp()
 
 
-# Example: aoc p 1 2019
+# Example: aoc pit 1 2019
 when isMainModule:
   try:
-    runCommand(commandLineParams())
+    runCommands(commandLineParams())
   except IOError as e:
     echo "ERROR: " & e.msg
