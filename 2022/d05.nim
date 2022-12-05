dd1486c4d9d12adb2d821f8f1728f9d8a5279049
Remigijus Jodelis <remigijus@toptal.com>
2022-12-05 11:30:40 +0200

    Day 5 of 2022

---
+++ 2022/d05.nim
@@ -0,0 +1,48 @@
+# Advent of Code 2022 - Day 5
+
+import std/[strutils,strscans,algorithm]
+
+type
+  Move = tuple[num, src, dst: int]
+
+  Stacks = array[10, seq[char]]
+
+  Data = object
+    stacks: Stacks
+    moves: seq[Move]
+
+
+proc parseData: Data =
+  var a, b, c: int
+  for line in readAll(stdin).strip.splitLines:
+    if line.len > 0 and line[0] == '[':
+      for i in 1..9:
+        let c = line[i*4-3]
+        if c != ' ': result.stacks[i].insert(c)
+
+    elif line.scanf("move $i from $i to $i", a, b, c):
+      result.moves.add (a, b, c)
+
+
+proc moveCrates(data: Data, multi = false): Stacks =
+  result = data.stacks
+  for (num, src, dst) in data.moves:
+    for i in 0..<num:
+      result[dst].add result[src].pop
+    if multi:
+      let l = result[dst].len
+      result[dst].reverse(l-num, l-1)
+
+
+proc top(stacks: Stacks): string =
+  for s in stacks[1..^1]:
+    result.add s[^1]
+
+
+let data = parseData()
+
+let part1 = data.moveCrates.top
+let part2 = data.moveCrates(true).top
+
+echo part1
+echo part2
