b7af2e82fcf017d929dee62ee4b5234778de421a
Remigijus Jodelis <remigijus@toptal.com>
2022-12-25 12:34:45 +0200

    Day 25 of 2022

---
+++ 2022/d25.nim
@@ -0,0 +1,32 @@
+# Advent of Code 2022 - Day 25
+
+import std/[strutils,sequtils]
+
+const digits = "=-012"
+
+type Data = seq[int]
+
+
+func parseSnafu(line: string): int =
+  line.mapIt(digits.find(it)-2).foldl(a * 5 + b)
+
+
+proc parseData: Data =
+  readAll(stdin).strip.splitLines.map(parseSnafu)
+
+
+func toSnafu(n: int): string =
+  var n = n
+  var d = 0
+  while n != 0:
+    d = n mod 5
+    n = n div 5
+    if d > 2:
+      d -= 5
+      n += 1
+    result.insert $digits[d+2]
+
+
+let data = parseData()
+
+echo data.foldl(a + b).toSnafu
