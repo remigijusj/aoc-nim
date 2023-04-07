65b8c1634de1d9ffcae430288521c819b7c5133e
Remigijus Jodelis <remigijus@toptal.com>
2023-04-07 20:17:53 +0300

    Day 3 of 2018

---
+++ 2018/d03.nim
@@ -0,0 +1,58 @@
+# Advent of Code 2018 - Day 3
+
+import std/[strscans,strutils,sequtils,tables,sugar]
+
+type
+  Cell = tuple[x, y: int]
+
+  Claim = object
+    id: int
+    x, y: int
+    w, h: int
+
+  Data = seq[Claim]
+
+  Grid = CountTable[Cell]
+
+
+func parseClaim(line: string): Claim =
+  assert line.scanf("#$i @ $i,$i: $ix$i", result.id, result.x, result.y, result.w, result.h)
+
+
+proc parseData: Data =
+  readAll(stdin).strip.splitLines.map(parseClaim)
+
+
+iterator cells(claim: Claim): Cell =
+  for x in claim.x ..< claim.x + claim.w:
+    for y in claim.y ..< claim.y + claim.h:
+      yield (x, y)
+
+
+func buildGrid(data: Data): Grid =
+  for claim in data:
+    for cell in claim.cells:
+      result.inc(cell)
+
+
+func overlapArea(grid: Grid): int =
+  for cnt in grid.values:
+    if cnt > 1:
+      result.inc
+
+
+func nonOverlappingClaim(data: Data, grid: Grid): int =
+  for claim in data:
+    var overlap = false
+    for cell in claim.cells:
+      if grid.getOrDefault(cell) > 1:
+        overlap = true
+    if not overlap:
+      return claim.id
+
+
+let data = parseData()
+let grid = data.buildGrid
+
+echo grid.overlapArea
+echo data.nonOverlappingClaim(grid)
