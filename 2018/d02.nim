aab7b3e1e509c97c8859f98c4fdf3082e7ada126
Remigijus Jodelis <remigijus@toptal.com>
2023-04-07 16:12:13 +0300

    Day 2 of 2018

---
+++ 2018/d02.nim
@@ -0,0 +1,51 @@
+# Advent of Code 2018 - Day 2
+
+import std/[strutils,sequtils,tables,sugar]
+
+type Data = seq[string]
+
+
+proc parseData: Data =
+  readAll(stdin).strip.splitLines
+
+
+func countChars(line: string): CountTable[char] =
+  for c in line: result.inc(c)
+
+
+func calcChecksum(data: Data): int =
+  var twos, threes: int
+
+  for line in data:
+    let val = line.countChars.values.toSeq
+    if 2 in val: twos.inc
+    if 3 in val: threes.inc
+
+  twos * threes
+
+
+func difference(a, b: string): int =
+  for k in 0 ..< a.len:
+    if a[k] != b[k]:
+      result.inc
+
+
+func matchingChars(a, b: string): string =
+  for k in 0 ..< a.len:
+    if a[k] == b[k]:
+      result.add(a[k])
+
+
+func findCorrectPair(data: Data): string =
+  for i in 0 ..< data.len:
+    for j in 0 ..< i:
+      let a = data[i]
+      let b = data[j]
+      if difference(a, b) == 1:
+        return matchingChars(a, b)
+
+
+let data = parseData()
+
+echo data.calcChecksum
+echo data.findCorrectPair
