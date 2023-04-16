b8ff35a8b5c51a996ad464f9046cb9a025459ec9
Remigijus Jodelis <remigijus@toptal.com>
2023-04-17 00:19:17 +0300

    Day 8 of 2018

---
+++ 2018/d08.nim
@@ -0,0 +1,41 @@
+# Advent of Code 2018 - Day 8
+
+import std/[strutils,sequtils]
+
+type Data = seq[int]
+
+
+proc parseData: Data =
+  readAll(stdin).strip.split(' ').map(parseInt)
+
+
+func fold(data: Data, value: bool): int =
+  var idx = 0
+
+  proc helper(): int =
+    let nodes = data[idx]
+    let metas = data[idx+1]
+    idx.inc(2)
+
+    var children: seq[int]
+    for _ in 0..<nodes:
+      let val = helper()
+      children.add(val)
+      if not value:
+        result += val
+
+    for _ in 0..<metas:
+      if value and nodes > 0:
+        if data[idx] in 1..children.len:
+          result += children[data[idx]-1]
+      else:
+        result += data[idx]
+      idx.inc
+
+  helper()
+
+
+let data = parseData()
+
+echo data.fold(false)
+echo data.fold(true)
