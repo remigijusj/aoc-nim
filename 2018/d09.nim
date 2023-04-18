500bf62964c99ff235a90bc9750e54ae1912b8d1
Remigijus Jodelis <remigijus@toptal.com>
2023-04-18 09:50:56 +0300

    Day 9 of 2018

---
+++ 2018/d09.nim
@@ -0,0 +1,36 @@
+# Advent of Code 2018 - Day 9
+
+import std/[strscans,strutils,lists]
+
+type Data = tuple[players, marbles: int]
+
+
+proc parseData: Data =
+  let data = readAll(stdin).strip
+  assert data.scanf("$i players; last marble is worth $i points", result.players, result.marbles)
+
+
+func maximumScore(players, marbles: int): int =
+  var scores = newSeq[int](players)
+  var circle = initDoublyLinkedRing[int]()
+  circle.add(0)
+
+  for marble in 1..marbles:
+    let player = marble mod players
+    if marble mod 23 == 0:
+      for _ in 1..6:
+        circle.head = circle.head.prev
+      let node = circle.head.prev
+      scores[player].inc(marble + node.value)
+      circle.remove(node)
+    else:
+      circle.head = circle.head.next.next
+      circle.prepend(marble)
+
+  result = scores.max
+
+
+let data = parseData()
+
+echo maximumScore(data.players, data.marbles)
+echo maximumScore(data.players, data.marbles * 100)
