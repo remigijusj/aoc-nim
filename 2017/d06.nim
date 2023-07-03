30e741737731732477df1376952114c32ee61ae3
Remigijus Jodelis <remigijus@toptal.com>
2023-07-03 17:45:26 +0300

    Day 6 of 2017

---
+++ 2017/d06.nim
@@ -0,0 +1,40 @@
+# Advent of Code 2017 - Day 6
+
+import std/[strutils,sequtils,tables]
+
+type
+  Data = seq[int]
+
+  Loop = Table[Data, int]
+
+
+proc parseData: Data =
+  readAll(stdin).strip.splitWhitespace.map(parseInt)
+
+
+proc redistribute(data: var Data) =
+  var idx = data.maxIndex
+  let num = data[idx]
+
+  data[idx] = 0
+  for i in 0..<num:
+    idx = (idx + 1) mod data.len
+    data[idx].inc
+
+
+func detectLoop(data: Data): (int, int) =
+  var data = data
+  var loop: Loop
+
+  while data notin loop:
+    loop[data] = loop.len
+    data.redistribute
+
+  result = (loop.len, loop.len - loop[data])
+
+
+let data = parseData()
+let loop = data.detectLoop
+
+echo loop[0]
+echo loop[1]
