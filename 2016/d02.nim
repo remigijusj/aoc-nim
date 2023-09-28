db715c705a6def75f475c62e7a87cb3c588999b2
Remigijus Jodelis <remigijus@toptal.com>
2023-09-28 11:49:15 +0300

    Day 2 of 2016

---
+++ 2016/d02.nim
@@ -0,0 +1,55 @@
+# Advent of Code 2016 - Day 2
+
+import std/[strutils,tables]
+import ../utils/common
+
+type
+  Data = seq[string]
+
+  XY = tuple[x, y: int]
+
+  Keypad = Table[XY, char]
+
+
+proc parseData: Data =
+  readAll(stdin).strip.splitLines
+
+
+converter toKeypad(rep: string): Keypad =
+  let lines = rep.split("|")
+  for y, line in lines:
+    for x, c in line:
+      if c != ' ': result[(x, y)] = c
+
+
+func find(keypad: Keypad, key: char): XY =
+  for xy, c in keypad:
+    if c == key:
+      return xy
+
+
+func move(pos: XY, code: char): XY =
+  case code
+    of 'U': result = (pos.x, pos.y-1)
+    of 'D': result = (pos.x, pos.y+1)
+    of 'L': result = (pos.x-1, pos.y)
+    of 'R': result = (pos.x+1, pos.y)
+    else: assert false
+
+
+func runCodes(data: Data, keypad: Keypad): string =
+  var this = keypad.find('5')
+  for codes in data:
+    for code in codes:
+      let next = this.move(code)
+      if next in keypad:
+        this = next
+
+    result &= keypad[this]
+
+
+let data = parseData()
+
+benchmark:
+  echo data.runCodes("123|456|789")
+  echo data.runCodes("  1  | 234 |56789| ABC |  D  ")
