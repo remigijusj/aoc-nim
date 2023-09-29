994c8445f487d7188433eb41a57024db112fded8
Remigijus Jodelis <remigijus@toptal.com>
2023-09-29 23:35:05 +0300

    Day 6 of 2016

---
+++ 2016/d06.nim
@@ -0,0 +1,28 @@
+# Advent of Code 2016 - Day 6
+
+import std/[strutils,sequtils,tables]
+import ../utils/common
+
+type
+  Data = seq[string]
+
+  Freq = CountTable[char]
+
+
+proc parseData: Data =
+  readAll(stdin).strip.splitLines
+
+
+func frequencies(data: Data): seq[Freq] =
+  newSeq(result, data[0].len)
+  for line in data:
+    for i, c in line:
+      result[i].inc(c)
+
+
+let data = parseData()
+
+benchmark:
+  let freq = data.frequencies
+  echo freq.mapIt(it.largest.key).join
+  echo freq.mapIt(it.smallest.key).join
