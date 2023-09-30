15156293fa2445721969be3e45bdcd04849f5f87
Remigijus Jodelis <remigijus@toptal.com>
2023-09-30 12:51:01 +0300

    Day 7 of 2016

---
+++ 2016/d07.nim
@@ -0,0 +1,27 @@
+# Advent of Code 2016 - Day 7
+
+import std/[strutils,sequtils,nre]
+import ../utils/common
+
+type Data = seq[string]
+
+
+proc parseData: Data =
+  readAll(stdin).strip.splitLines
+
+
+func supportTLS(line: string): bool =
+  line.contains(re"(\w)(?!\1)(\w)\2\1") and
+    not line.contains(re"(\w)(?!\1)(\w)\2\1\w*\]")
+
+
+func supportSSL(line: string): bool =
+  line.contains(re"(?:^|\])\w*?(\w)(?!\1)(\w)\1.*?\[\w*?\2\1\2") or
+    line.contains(re"\[\w*?(\w)(?!\1)(\w)\1.*?\]\w*\2\1\2")
+
+
+let data = parseData()
+
+benchmark:
+  echo data.countIt(it.supportTLS)
+  echo data.countIt(it.supportSSL)
