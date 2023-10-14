ea6a58048295ee10eff7273e70e79223e9e4957b
Remigijus Jodelis <remigijus@toptal.com>
2023-10-14 22:37:18 +0300

    Day 18 of 2016

---
+++ 2016/d18.nim
@@ -0,0 +1,29 @@
+# Advent of Code 2016 - Day 18
+
+import std/[strutils]
+import ../utils/common
+
+const traps = ["^^.", ".^^", "^..", "..^"]
+
+proc parseData: string =
+  readAll(stdin).strip
+
+
+proc nextGen(row: var string) =
+  var prev = '.' & row & '.'
+  for n in 0..<row.len:
+    row[n] = (if prev[n..n+2] in traps: '^' else: '.')
+
+
+func countSafeTiles(data: string, rows: int): int =
+  var row = data
+  for _ in 1..rows:
+    result += row.count('.')
+    row.nextGen
+
+
+let data = parseData()
+
+benchmark:
+  echo data.countSafeTiles(40)
+  echo data.countSafeTiles(400000)
