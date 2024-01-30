bbdc3f6a1aadf351c349c7f5dce4d810f4329394
Remigijus Jodelis <remigijus@toptal.com>
2024-01-30 21:25:53 +0200

    Day 5 of 2015

---
+++ 2015/d05.nim
@@ -0,0 +1,27 @@
+# Advent of Code 2015 - Day 5
+
+import std/[strutils,sequtils,nre]
+import ../utils/common
+
+type Data = seq[string]
+
+const vowels = {'a','e','i','o','u'}
+
+
+proc parseData: Data =
+  readAll(stdin).strip.splitLines
+
+
+func nice1(line: string): bool =
+  line.count(vowels) >= 3 and line.contains(re"(.)\1") and not line.contains(re"ab|cd|pq|xy")
+
+
+func nice2(line: string): bool =
+  line.contains(re"(..).*\1") and line.contains(re"(.).\1")
+
+
+let data = parseData()
+
+benchmark:
+  echo data.countIt(it.nice1)
+  echo data.countIt(it.nice2)
