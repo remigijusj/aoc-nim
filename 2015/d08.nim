a8265dcf21a84d7c4784e3396596f666e3d5ffe4
Remigijus Jodelis <remigijus@toptal.com>
2024-02-11 21:29:55 +0200

    Day 8 of 2015

---
+++ 2015/d08.nim
@@ -0,0 +1,25 @@
+# Advent of Code 2015 - Day 8
+
+import std/[strutils,sequtils]
+import ../utils/common
+
+type Data = seq[string]
+
+
+proc parseData: Data =
+  readAll(stdin).strip.splitLines
+
+
+func delta1(line: string): int =
+  result = line.len - line.unescape.len
+
+
+func delta2(line: string): int =
+  result = line.escape.len - line.len
+
+
+let data = parseData()
+
+benchmark:
+  echo data.map(delta1).sum
+  echo data.map(delta2).sum
