c3e38a418e98dc103ac7071c9250260103105d55
Remigijus Jodelis <remigijus@toptal.com>
2024-02-13 19:10:36 +0200

    Day 10 of 2015

---
+++ 2015/d10.nim
@@ -0,0 +1,40 @@
+# Advent of Code 2015 - Day 10
+
+import std/[strutils]
+from math import round
+import ../utils/common
+
+
+proc parseData: string =
+  readAll(stdin).strip
+
+
+func lookAndSay(data: string): string =
+  let cap = round(data.len.float * 1.31).int
+  result = newStringOfCap(cap)
+
+  var digit = data[0]
+  var count = 1
+  for i in 1..<data.len:
+    if data[i] == digit:
+      count.inc
+    else:
+      result.add($count)
+      result.add($digit)
+      digit = data[i]
+      count = 1
+  result.add($count)
+  result.add($digit)
+
+
+proc extendTimes(data: var string, times: int): int =
+  for i in 1..times:
+    data = data.lookAndSay
+  result = data.len
+
+
+var data = parseData()
+
+benchmark:
+  echo data.extendTimes(40)
+  echo data.extendTimes(10)
