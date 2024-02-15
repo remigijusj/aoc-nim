c51cac8bad2b6181721b08ba6b7a5c99adf479aa
Remigijus Jodelis <remigijus@toptal.com>
2024-02-15 18:31:07 +0200

    Day 12 of 2015

---
+++ 2015/d12.nim
@@ -0,0 +1,31 @@
+# Advent of Code 2015 - Day 12
+
+import std/[strutils,tables,json]
+import ../utils/common
+
+
+proc parseData: JsonNode =
+  readAll(stdin).strip.parseJson
+
+
+func sumNumbers(node: JsonNode, skipRed: bool): int =
+  case node.kind
+  of JInt:
+    result += node.num
+  of JArray:
+    for sub in node.elems:
+      result += sub.sumNumbers(skipRed)
+  of JObject:
+    for sub in node.fields.values:
+      result += sub.sumNumbers(skipRed)
+      if skipRed and sub.kind == JString and sub.str == "red":
+        return 0
+  else:
+    discard
+
+
+let data = parseData()
+
+benchmark:
+  echo data.sumNumbers(false)
+  echo data.sumNumbers(true)
