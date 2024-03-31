25fcf755faf699c19bf057e6c032569cfd3f679a
Remigijus Jodelis <remigijus@toptal.com>
2024-03-31 10:42:21 +0300

    Day 24 of 2015

---
+++ 2015/d24.nim
@@ -0,0 +1,53 @@
+# Advent of Code 2015 - Day 24
+
+import std/[strutils,sequtils]
+import ../utils/common
+
+type Data = seq[int]
+
+
+proc parseData: Data =
+  readAll(stdin).strip.splitLines.map(parseInt)
+
+
+func idealSplit(data: Data, target, size: int): int =
+  var indices: seq[int] = (0..<size).toSeq
+  var weight: int = data[0..<size].sum
+
+  while true:
+    if weight == target:
+      return indices.mapIt(data[it]).foldl(a * b) # QE
+
+    var depth = size - 1
+    while indices[depth] == data.len - size + depth:
+      if depth == 0:
+        return -1 # not found
+      depth -= 1
+
+    let this = indices[depth]
+    let next = indices[depth] + 1
+    indices[depth] = next
+    weight = weight - data[this] + data[next]
+    depth += 1
+
+    while depth < size:
+      let this = indices[depth]
+      let next = indices[depth - 1] + 1
+      indices[depth] = next
+      weight = weight - data[this] + data[next]
+      depth += 1
+
+
+func idealSplit(data: Data, parts: int): int =
+  let weight = data.sum div parts
+  for size in 1..<data.len:
+    result = data.idealSplit(weight, size)
+    if result >= 0:
+      return
+
+
+let data = parseData()
+
+benchmark:
+  echo data.idealSplit(3)
+  echo data.idealSplit(4)
